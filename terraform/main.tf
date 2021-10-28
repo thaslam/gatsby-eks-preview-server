2a56c2693cdbbfe78a94945f1832135dc0c1afe1
Tom Haslam <tom@Toms-MacBook-Pro.local>
2021-10-27 22:32:00 -0400

    create dockerfile to host preview server

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ locals {
 ################################################################################
 
 module "eks" {
-  source = "../.."
+  source          = "terraform-aws-modules/eks/aws"
 
   cluster_name    = local.name
   cluster_version = local.cluster_version
@@ -160,5 +160,66 @@ module "vpc" {
     Example    = local.name
     GithubRepo = "terraform-aws-eks"
     GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+# deploy docker hub image (this shoudl be moved out into its own module)
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleResumeApp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/nginx_status"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
   }
 }
